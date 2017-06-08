e0ff1e90e0d53ab6b8270c78cf348d20d8586850
Seth Vargo <seth@sethvargo.com>
2017-06-07 22:19:34 -0400

    Handle arrays in stringify keys

--- lib/nomad/stringify.rb
+++ lib/nomad/stringify.rb
@@ -8,6 +8,7 @@ module Nomad
     # @return [Hash]
     def stringify_keys(hash)
       (hash || {}).inject({}) do |h, (key, value)|
+        value = value.map { |h| stringify_keys(h) } if value.is_a?(Array)
         value = stringify_keys(value) if value.is_a?(Hash)
         h[key.to_s] = value
         h
